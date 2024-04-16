5bf14e314190be8d95864380b21274c8162aade5
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-04-16 23:23:05 +0530

    'Cards - 3D' >> 'Card 01' is added

---
+++ Cards - 3D/Cards 01/Cards.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelectorAll(".main-content"), {
+    max: 20,
+    speed: 5000,
+    scale: 1.25,
+    speed: 3000,
+    glare: true,  // if it should have a "glare" effect
+});
