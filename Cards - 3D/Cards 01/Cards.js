e5f9438619177ecda185b25053197d5b3bf770bc
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-04-16 23:59:28 +0530

    Make changes in Card 01 and added another file with 'Cursor Loading Animation'.

--- Cards - 3D/Cards 01/Cards.js
+++ Cards - 3D/Cards 01/Cards.js
@@ -1,7 +1,8 @@
 VanillaTilt.init(document.querySelectorAll(".main-content"), {
     max: 20,
-    speed: 5000,
-    scale: 1.25,
+    speed: 2000,
+    scale: 1.05,
     speed: 3000,
     glare: true,  // if it should have a "glare" effect
+    "max-glare": 0.1,
 });
